65ee9a454e788a25a578d04c6d78150ed3fe5a41
apidatabasesync <apidatabasetest@outlook.dk>
2016-10-05 15:30:12 +0200

    First commit

---
+++ EconomicsApi/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace EconomicsApi
+{
+    partial class emailSync
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 20);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Email";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(177, 36);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 115);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 27);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "CostumerName";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(177, 122);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // emailSync
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 323);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "emailSync";
+            this.Text = "Master Sync";
+            this.Load += new System.EventHandler(this.emailSync_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ EconomicsApi/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EconSoapLibrary.EconApi;
+
+
+namespace EconomicsApi
+{
+    public partial class emailSync : Form
+    {
+        public emailSync()
+        {
+            InitializeComponent();
+
+           
+        }
+
+        private void emailSync_Load(object sender, EventArgs e)
+        {
+            //JiCekRM3yza8AnDJgprYsruI1w9isZnRYpILl3AVbIo1
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //textBox1.Text = "test";
+
+            var session = new EconSoapLibrary.EconApi.EconomicWebServiceSoapClient();
+            session.ConnectWithToken("bual5TKRrDC9709Ci71cVKCpkMUpYk33nzlxkIfmnl01", "VFMBs45gNtceuBWMKk8EHnY92KNj6Xye838h8252uQU1");
+            var company = session.Company_Get();
+            textBox1.Text = company.Number;
+            //Console.WriteLine(company.Number);
+            session.Disconnect();
+
+            
+            
+             
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var session2 = new EconSoapLibrary.EconApi.EconomicWebServiceSoapClient();
+            session2.ConnectWithToken("bual5TKRrDC9709Ci71cVKCpkMUpYk33nzlxkIfmnl01", "VFMBs45gNtceuBWMKk8EHnY92KNj6Xye838h8252uQU1");
+            var name = session2.
+            textBox2.Text = name.Length.ToString();
+            session2.Disconnect();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
